b04ea93177c12f5c4860d391ef0134c9ff164191
Kues <BKU064@crb.apmoller.net>
2017-08-09 18:25:48 +0200

    Initial test commit of one file for overbooking

---
+++ Overbooking/tune_logistic_regression.R
@@ -0,0 +1,12 @@
+# tune the parameters of the logistic regression model used for overbooking
+
+
+# read in appropriate data
+
+# format data as required for logistic regerssion
+
+# run logistic regression
+
+# parse results as desired
+
+# output results

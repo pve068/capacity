ec5d75a82ab39a845c40c23a27fd170cb03d638b
Kues <BKU064@crb.apmoller.net>
2017-08-09 22:53:43 +0200

    Test connect to db and query from table

--- Overbooking/tune_logistic_regression.R
+++ Overbooking/tune_logistic_regression.R
@@ -1,9 +1,15 @@
 # tune the parameters of the logistic regression model used for overbooking
-
+library(RODBC)
 
 # read in appropriate data
+con <- odbcDriverConnect(connection="driver={SQL Server};server=scrbsqlaze041;
+                         database=analyticsdatamart;uid=raappuser;
+                         pwd=C0ntainers1@;")
 
-# format data as required for logistic regerssion
+res <- sqlQuery(con, "select top 1000 * from dbo.booking_snapshot")
+odbcClose(con)
+
+# format data as required for logistic regression
 
 # run logistic regression
 

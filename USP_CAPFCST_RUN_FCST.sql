935585aa40f9d67ffe42f49369e32bb0fa3ca324
Marshall <EMA197@crb.apmoller.net>
2017-08-30 17:07:57 -0400

    Updated logic to handle new booking final layout

--- USP_CAPFCST_RUN_FCST.sql
+++ USP_CAPFCST_RUN_FCST.sql
@@ -1,3 +1,9 @@
+USE [CapInvRepository]
+
+IF ( OBJECT_ID('dbo.USP_CAPFCST_RUN_FCST', 'P') IS NOT NULL ) 
+   DROP PROCEDURE dbo.USP_CAPFCST_RUN_FCST
+GO
+
 CREATE PROCEDURE [DBO].[USP_CAPFCST_RUN_FCST]
 /****************************************************************************************************
 * COPYRIGHT REVENUE ANALYTICS 2017
@@ -39,7 +45,7 @@ CREATE PROCEDURE [DBO].[USP_CAPFCST_RUN_FCST]
 														(RUNDATE)          
 ****************************************************************************************************/
 	 @V_RUNDATE DATE = NULL
-	 ,@V_SERVICE VARCHAR(10)
+	,@V_SERVICE VARCHAR(10)
 	,@V_PARENT_PROCESS_ID INT = 0
 	,@V_ERROR_CODE INT OUTPUT
 AS
@@ -50,26 +56,136 @@ BEGIN
 	DECLARE @V_MSG  NVARCHAR(4000)
 	DECLARE @V_NOTE  NVARCHAR(4000)
 	DECLARE @V_ERRORSEVERITY INT  
-
 	DECLARE @V_PROCESS_ID INT = NEXT VALUE FOR DBO.SEQ_LOG_PROCESS_ID
 	DECLARE @V_PROCESS_NAME VARCHAR(255) = OBJECT_SCHEMA_NAME(@@PROCID) + '.' + OBJECT_NAME(@@PROCID)
 	DECLARE @V_FIANLERROR INT
 	DECLARE @V_SYSTEM_USER NVARCHAR(255) = SYSTEM_USER
 	DECLARE @V_SYSTEMTIME DATETIME = GETUTCDATE()
 	DECLARE @V_RECORD_CNT INT
-
 	DECLARE @V_STEP_ID TINYINT
 	DECLARE @V_STEP_NAME VARCHAR(255)
 	DECLARE @V_AUDITVERSIONID INT
 	DECLARE @RETURN_VALUE INT
 
-	EXEC	@RETURN_VALUE = [DBO].[USP_CAPFCST_AD_CONSUMPTION]
-						@V_RUNDATE = @V_RUNDATE,
-						@V_SERVICE = @V_SERVICE,
-						@V_ERROR_CODE = @V_ERROR_CODE OUTPUT
-	
-	EXEC	@RETURN_VALUE = [DBO].USP_CAPFCST_CALC_FREESALECONSUMPTION
-						@V_RUNDATE = @V_RUNDATE,
-						@V_ERROR_CODE = @V_ERROR_CODE OUTPUT
+	BEGIN TRY 
+		--LOG PROCESS		
+		EXEC DBO.USP_LOG_PROCESS @V_PROCESS_ID = @V_PROCESS_ID
+							, @V_PROCESS_NAME = @V_PROCESS_NAME
+							, @V_USER_NAME = @V_SYSTEM_USER
+							, @V_START_DT = @V_SYSTEMTIME
+							, @V_END_DT = NULL
+							, @V_STATUS = 'RUNNING'
+							, @V_ACTION = 1
+							, @V_PARENT_PROCESS_ID = 0
+							, @V_NOTE = 'Process CapFcst'
+							, @V_START_STEP = 1;
 
+			SET @V_STEP_ID = 10
+			SET @V_STEP_NAME = 'Build consumption table -- Run Stored Proc [DBO].[USP_CAPFCST_AD_CONSUMPTION]'
+			EXEC dbo.USP_LOG_PROCESS_DETAIL @V_PROCESS_ID = @V_PROCESS_ID
+  						, @V_STEP_ID = @V_STEP_ID
+  						, @V_STEP_NAME = @V_STEP_NAME
+  						, @V_START_DT = @V_SYSTEMTIME
+  						, @V_END_DT = NULL
+  						, @V_STATUS = 'RUNNING'
+  						, @V_ROWS_PROCESSED = NULL
+  						, @V_ACTION = 1
+  						, @V_NOTE = 'Using booking final build consumption table at the leg level';
+
+			EXEC	@RETURN_VALUE = [DBO].[USP_CAPFCST_AD_CONSUMPTION]
+								@V_RUNDATE = @V_RUNDATE,
+								@V_SERVICE = @V_SERVICE,
+								@V_PARENT_PROCESS_ID = @V_PROCESS_ID,
+								@V_ERROR_CODE = @V_ERROR_CODE OUTPUT
+			IF 	@RETURN_VALUE = 0
+			BEGIN
+				--UPdate Detail 
+				EXEC dbo.USP_LOG_PROCESS_DETAIL @V_PROCESS_ID = @V_PROCESS_ID
+  							, @V_STEP_ID = @V_STEP_ID
+  							, @V_STEP_NAME = @V_STEP_NAME
+  							, @V_START_DT = NULL
+  							, @V_END_DT = @V_SYSTEMTIME
+  							, @V_STATUS = 'COMPLETED'
+  							, @V_ROWS_PROCESSED = NULL
+  							, @V_ACTION = 2
+  							, @V_NOTE = NULL;
+				END
+
+			SET @V_STEP_ID = 20
+			SET @V_STEP_NAME = 'Generate Capacity Forecast'
+			EXEC dbo.USP_LOG_PROCESS_DETAIL @V_PROCESS_ID = @V_PROCESS_ID
+  						, @V_STEP_ID = @V_STEP_ID
+  						, @V_STEP_NAME = @V_STEP_NAME
+  						, @V_START_DT = @V_SYSTEMTIME
+  						, @V_END_DT = NULL
+  						, @V_STATUS = 'RUNNING'
+  						, @V_ROWS_PROCESSED = NULL
+  						, @V_ACTION = 1
+  						, @V_NOTE = 'Generate Capacity Forecast final table';
+
+			EXEC	@RETURN_VALUE = [DBO].USP_CAPINV_CALC_REMAINING_CAPACITY
+								@V_RUNDATE = @V_RUNDATE,
+								@V_PARENT_PROCESS_ID = @V_PROCESS_ID,
+								@V_ERROR_CODE = @V_ERROR_CODE OUTPUT
+
+			IF 	@RETURN_VALUE <> 0
+			BEGIN
+				--UPdate Detail 
+				EXEC dbo.USP_LOG_PROCESS_DETAIL @V_PROCESS_ID = @V_PROCESS_ID
+  							, @V_STEP_ID = @V_STEP_ID
+  							, @V_STEP_NAME = @V_STEP_NAME
+  							, @V_START_DT = NULL
+  							, @V_END_DT = @V_SYSTEMTIME
+  							, @V_STATUS = 'COMPLETED'
+  							, @V_ROWS_PROCESSED = NULL
+  							, @V_ACTION = 2
+  							, @V_NOTE = NULL;
+			END
+
+			--Update Process --Complete Process
+			EXEC dbo.USP_LOG_PROCESS @V_PROCESS_ID = @V_PROCESS_ID
+								, @V_PROCESS_NAME = @V_PROCESS_NAME
+								, @V_USER_NAME = @V_SYSTEM_USER
+								, @V_START_DT = NULL
+								, @V_END_DT = @V_SYSTEMTIME
+								, @V_STATUS = 'COMPLETED'
+								, @V_ACTION = 2
+								, @V_PARENT_PROCESS_ID = NULL
+								, @V_NOTE = NULL
+								, @V_START_STEP = 1;
+	END TRY
+	BEGIN CATCH
+		SET @V_ERRORMESSAGE = 'Process to refresh UDF Booking Analysis dataset failed: ' + ERROR_MESSAGE()
+		SET @V_ERRORSEVERITY = ERROR_SEVERITY()  
+		SET @V_SYSTEMTIME = GETUTCDATE()
+
+		--UPdate Detail 
+		EXEC dbo.USP_LOG_PROCESS_DETAIL @V_PROCESS_ID = @V_PROCESS_ID
+  					, @V_STEP_ID = @V_STEP_ID
+  					, @V_STEP_NAME = @V_STEP_NAME
+  					, @V_START_DT = NULL
+  					, @V_END_DT = @V_SYSTEMTIME
+  					, @V_STATUS = 'FAILED'
+  					, @V_ROWS_PROCESSED = NULL
+  					, @V_ACTION = 2
+  					, @V_NOTE = @V_ERRORMESSAGE
+  					;
+
+		--Update Process --Complete Process
+		EXEC dbo.USP_LOG_PROCESS @V_PROCESS_ID = @V_PROCESS_ID
+							, @V_PROCESS_NAME = @V_PROCESS_NAME
+							, @V_USER_NAME = @V_SYSTEM_USER
+							, @V_START_DT = NULL
+							, @V_END_DT = @V_SYSTEMTIME
+							, @V_STATUS = 'FAILED'
+							, @V_ACTION = 2
+							, @V_PARENT_PROCESS_ID = NULL
+							, @V_NOTE =  @V_ERRORMESSAGE
+							, @V_START_STEP = 1
+							;  
+
+		SET @V_ERROR_CODE = -1
+		RETURN @V_ERROR_CODE
+	END CATCH
 END
+
